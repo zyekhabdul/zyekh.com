1993c9e2883358eb77a4fa29644ebc9b5347e061
zyekhabdul <zyekhabdulqadirjailani@gmail.com>
2026-08-03 15:23:45 +0700

    fix(sw): add controllerchange auto-reload + bump v71

--- assets/js/site-nav.js
+++ assets/js/site-nav.js
@@ -43,7 +43,10 @@ class SiteNav extends HTMLElement {
     // Centralized Service Worker Registration
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(reg => reg.update()).catch(() => {});
+        navigator.serviceWorker.register("/sw.js").then(reg => {
+          reg.update();
+          navigator.serviceWorker.addEventListener("controllerchange", () => window.location.reload());
+        }).catch(() => {});
       });
     }
 
