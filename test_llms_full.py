f241c61cf61ad24e8f916339f3689dfe230ba541
zyekhabdul <zyekhabdulqadirjailani@gmail.com>
2026-08-12 19:34:47 +0700

    chore(clean): remove pycache git tracking, add gitignore rules, and remove 5 redundant one-off scripts

--- test_llms_full.py
+++
@@ -1,6 +0,0 @@
-import os
-import glob
-from bs4 import BeautifulSoup # Maybe use regex instead if bs4 not available
-
-# Actually we can just read the HTML files or Markdown source files?
-# Where does the blog content come from?
